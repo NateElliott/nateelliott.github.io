4a7cfa5c2b7469e647b0f9bd722a6465003d2d02
Nate Elliott <nelliott.ca@gmail.com>
2016-08-25 09:05:24 -0300

    wqertyuiop

--- stage.bat
+++ stage.bat
@@ -3,10 +3,10 @@
 set /p message="commit message: "
 set /p repo="repo(master): "
 if "%repo%" == "" set repo=master
-
+echo Working.
 git add --all
 git commit -m %message%
 git push -u origin %repo%
 
-echo
+
 echo Done!
