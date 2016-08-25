ce1fe563250bf7ee22750bc7f688c7c130358a6a
Nate Elliott <nelliott.ca@gmail.com>
2016-08-25 09:03:07 -0300

    test2

--- stage.bat
+++ stage.bat
@@ -1,4 +1,7 @@
 set /p message="commit message: "
+set /p repo="repo(master): "
+if "%repo%" == "" set repo=master
+
 git add --all
 git commit -m %message%
-git push -u origin master
+git push -u origin %repo%
