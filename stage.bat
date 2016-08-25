531040db1404aaa26ed9742b631c79008f8f6f74
Nate Elliott <nelliott.ca@gmail.com>
2016-08-25 10:22:23 -0300

    yes please

--- stage.bat
+++ stage.bat
@@ -1,6 +1,6 @@
 @echo off
-set /p message="commit message: "
-echo %message% > index.html
+set /p message="log: "
+echo [%date%-%time%] %message% 
 git add --all
 git commit -m "%message%"
 git push -u origin master
